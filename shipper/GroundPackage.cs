135fc81cbf4ea3665ce3816df6c3e2a700ad56e1
Brandon Berney <brandonberney@protonmail.com>
2019-04-17 16:41:09 -0400

    GroundPackage: introduce all logic

--- shipper/GroundPackage.cs
+++ shipper/GroundPackage.cs
@@ -1,12 +1,161 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace shipper
 {
     class GroundPackage
     {
+        private int _origin,
+            _destination;
+
+        private double _length,
+            _width,
+            _height,
+            _weight;
+
+        private const double MIN_DIMENSION = 0,
+            DEFAULT_DIMENSION = 1;
+
+        public int ZoneDistance
+        {
+            get
+            {
+                const int divider = 10000;
+                int larger,
+                    smaller,
+                    distance;
+
+                larger = Math.Max(OriginZip, DestinationZip);
+                smaller = Math.Min(OriginZip, DestinationZip);
+
+                distance = (larger / divider) - (smaller / divider);
+
+                return distance;
+            }
+        }
+
+        public int OriginZip
+        {
+            get
+            {
+                return _origin;
+            }
+            set
+            {
+                const int DEFAULT_ZIP = 40202;
+
+                if (IsValidZIP(value))
+                    _origin = value;
+                else
+                    _origin = DEFAULT_ZIP;
+            }
+        }
+
+        public int DestinationZip
+        {
+            get
+            {
+                return _destination;
+            }
+            set
+            {
+                const int DEFAULT_ZIP = 90210;
+
+                if (IsValidZIP(value))
+                    _destination = value;
+                else
+                    _destination = DEFAULT_ZIP;
+            }
+        }
+
+        public double Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                if (value > MIN_DIMENSION)
+                    _length = value;
+                else
+                    _length = DEFAULT_DIMENSION;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if (value > MIN_DIMENSION)
+                    _width = value;
+                else
+                    _width = DEFAULT_DIMENSION;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (value > MIN_DIMENSION)
+                    _height = value;
+                else
+                    _height = DEFAULT_DIMENSION;
+            }
+        }
+
+        public double Weight
+        {
+            get
+            {
+                return _weight;
+            }
+            set
+            {
+                if (value > MIN_DIMENSION)
+                    _weight = value;
+                else
+                    _weight = DEFAULT_DIMENSION;
+            }
+        }
+
+        private bool IsValidZIP(int zip)
+        {
+            int ZIP_MIN = 0,
+            ZIP_MAX = 99999;
+
+            return (zip >= ZIP_MIN && zip <= ZIP_MAX);
+        }
+
+        private double CalcCost()
+        {
+            const double DIMENSION_COST = .25,
+                DISTANCE_COST = .45;
+            const int MIN_DISTANCE = 1;
+
+            double cost;
+            cost = (DIMENSION_COST * (Length + Width + Height)) + (DISTANCE_COST * (ZoneDistance + MIN_DISTANCE) * Weight);
+
+            return cost;
+        }
+
+        public override string ToString()
+        {
+            return "Origin ZIP: {OriginZip:F5}{Environment.NewLine}" +
+                "Destination ZIP: {DestinationZip:F5}{Environment.NewLine}" +
+                "Zone Distance: {ZoneDistance}{Environment.NewLine}" +
+                "Length: {Length}{Environment.NewLine}" +
+                "Width: {Width}{Environment.NewLine}" +
+                "Height: {Height}{Environment.NewLine}" +
+                "Weight: {Weight}{Environment.NewLine}" +
+                "Cost: {CalcCost()}";
+        }
     }
 }
